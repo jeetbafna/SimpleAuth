f3e9f4d9040c65a6f1fd0dbb3aad350654122513
jeetbafna <jeetanil97@gmail.com>
2017-03-02 01:11:30 +0530

    Initial commit

---
+++ app/src/main/java/com/example/admin/codestrike/ProfileActivity.java
@@ -0,0 +1,110 @@
+package com.example.admin.codestrike;
+
+/**
+ * Created by admin on 01/03/2017.
+ */
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.Window;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+
+public class ProfileActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //firebase auth object
+    private FirebaseAuth firebaseAuth;
+
+    //view objects
+    private TextView textViewUserEmail;
+    private Button buttonLogout;
+    private DatabaseReference databaseReference;
+
+    //our new views
+    private EditText editTextName, editTextAddress;
+    private Button buttonSave;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        //initializing firebase authentication object
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        //if the user is not logged in
+        //that means current user will return null
+        if(firebaseAuth.getCurrentUser() == null){
+            //closing this activity
+            finish();
+            //starting login activity
+            startActivity(new Intent(this, LoginActivity.class));
+        }
+
+        //getting current user
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+
+        //initializing views
+        textViewUserEmail = (TextView) findViewById(R.id.textViewUserEmail);
+        buttonLogout = (Button) findViewById(R.id.buttonLogout);
+        editTextAddress = (EditText) findViewById(R.id.editTextAddress);
+        editTextName = (EditText) findViewById(R.id.editTextName);
+        buttonSave = (Button) findViewById(R.id.buttonSave);
+
+
+        //displaying logged in user name
+        textViewUserEmail.setText("Welcome "+user.getEmail());
+
+
+        //adding listener to button
+        buttonLogout.setOnClickListener(this);
+    }
+    private void saveUserInformation() {
+        //Getting values from database
+        String name = editTextName.getText().toString().trim();
+        String add = editTextAddress.getText().toString().trim();
+
+        //creating a userinformation object
+        UserInformation userInformation = new UserInformation(name, add);
+
+        //getting the current logged in user
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+
+        databaseReference.child(user.getUid()).setValue(userInformation);
+
+        //displaying a success toast
+        Toast.makeText(this, "Information Saved...", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onClick(View view) {
+        //if logout is pressed
+        if(view == buttonLogout){
+            //logging out the user
+            firebaseAuth.signOut();
+            //closing activity
+            finish();
+            //starting login activity
+            startActivity(new Intent(this, LoginActivity.class));
+        }
+        if(view == buttonSave){
+            saveUserInformation();
+        }
+    }
+}
+
---
+++ app/src/main/java/com/example/admin/codestrike/UserInformation.java
@@ -0,0 +1,17 @@
+package com.example.admin.codestrike;
+
+/**
+ * Created by admin on 02/03/2017.
+ */
+public class UserInformation {
+
+    public String name;
+    public String address;
+
+
+
+    public UserInformation(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+}
